980eb45d553eda821d45ab6b0f6dc986512b9902
shery <shery@DESKTOP-64HSMK0>
2021-10-08 14:15:21 +0300

    Добавьте файлы проекта.

---
+++ first_console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace first_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                values[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            foreach(int value in values)
+            {
+                Console.Write(value + " ");
+            } Console.WriteLine();
+
+            sortFunction(values);
+
+            foreach(int value in values)
+            {
+                Console.Write(value + " ");
+            }
+        }
+
+        static void sortFunction(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for(int k = i + 1; k < array.Length; k++)
+                {
+                    if(array[i] <= array[k])
+                    {
+                        int temp = array[i];
+                        array[i] = array[k];
+                        array[k] = temp;
+                        
+                    }
+                }
+            }
+        }
+    }
+}
